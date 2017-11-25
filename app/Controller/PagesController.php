8e0367f66626db54797a0b2c820742daa2bba817
anito <axelnitzschner@mac.com>
2017-11-25 19:44:34 +0100

    Minor changes

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -31,56 +31,56 @@ App::uses('AppController', 'Controller');
  */
 class PagesController extends AppController {
 
-/**
- * Controller name
- *
- * @var string
- */
-	public $name = 'Pages';
+    /**
+     * Controller name
+     *
+     * @var string
+     */
+    public $name = 'Pages';
 
-/**
- * Default helper
- *
- * @var array
- */
-	public $helpers = array('Html', 'Session');
+    /**
+     * Default helper
+     *
+     * @var array
+     */
+    public $helpers = array('Html', 'Session');
 
-/**
- * This controller does not use a model
- *
- * @var array
- */
-	public $uses = array();
+    /**
+     * This controller does not use a model
+     *
+     * @var array
+     */
+    public $uses = array();
 
- public function beforeFilter() {
-    $this->Auth->allowedActions = array('display');
-  }
- 
-/**
- * Displays a view
- *
- * @param mixed What page to display
- * @return void
- */
-	public function display() {
-		$path = func_get_args();
+    public function beforeFilter() {
+        $this->Auth->allowedActions = array('display');
+    }
 
-		$count = count($path);
-		if (!$count) {
-			$this->redirect('/');
-		}
-		$page = $subpage = $title_for_layout = null;
+    /**
+     * Displays a view
+     *
+     * @param mixed What page to display
+     * @return void
+     */
+    public function display() {
+        $path = func_get_args();
 
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-		$this->render(implode('/', $path));
-	}
+        $count = count($path);
+        if (!$count) {
+            $this->redirect('/');
+        }
+        $page = $subpage = $title_for_layout = null;
+
+        if (!empty($path[0])) {
+            $page = $path[0];
+        }
+        if (!empty($path[1])) {
+            $subpage = $path[1];
+        }
+        if (!empty($path[$count - 1])) {
+            $title_for_layout = Inflector::humanize($path[$count - 1]);
+        }
+        $this->set(compact('page', 'subpage', 'title_for_layout'));
+        $this->render(implode('/', $path));
+    }
 }
